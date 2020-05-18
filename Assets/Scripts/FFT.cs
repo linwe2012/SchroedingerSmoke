d2ba5fe32065b7dc3535fb6f37afd9bd967a87d0
leon <linwe2012@163.com>
2020-05-18 21:47:05 +0800

    isf three major functions done

--- Assets/Scripts/FFT.cs
+++ Assets/Scripts/FFT.cs
@@ -37,8 +37,11 @@ public class FFT : MonoBehaviour
     int kernelBlitIORT;
     int kernelBlitDebugOutput;
     int kernelIFFTConj;
+    
 
     int kernelBlitSliceOf3DTexture;
+    int kernelBlitSliceOf3DTextureFloat4;
+    int kernelBlitSliceOf3DTextureFloat1;
 
     int N;
     int FFTPow;
@@ -73,6 +76,7 @@ public class FFT : MonoBehaviour
         CS.SetTexture(kernelBlitIOTex, "OutputRT", rt);
         CS.Dispatch(kernelBlitIOTex, N / 8, N / 8, N / 8);
     }
+
     static public RenderTexture CreateRenderTexture(int size, RenderTextureFormat type = RenderTextureFormat.ARGBFloat)
     {
         RenderTexture rt = new RenderTexture(size, size, 0, type);
@@ -98,16 +102,16 @@ public class FFT : MonoBehaviour
         CS.Dispatch(kernelSampleSimplexFBMNoise, ChunkDivisions / 8, ChunkDivisions / 8, 1);
     }*/
 
-    public void fft(ref RenderTexture InputRT)
+    public void fft(ref RenderTexture InputRT, ref RenderTexture Output)
     {
-        
+        CS.SetInt("N", InputRT.width);
         //进行横向FFT
         for (int m = 1; m <= FFTPow; m++)
         {
             CS.SetInt("FFTPass", m);
             int ns = 1 << (m - 1);
             CS.SetInt("Ns", ns);
-            ComputeFFT(kernelFFTHorizontal, ref InputRT);
+            ComputeFFT(kernelFFTHorizontal, ref InputRT, ref Output);
         }
 
         //进行纵向FFT
@@ -116,7 +120,7 @@ public class FFT : MonoBehaviour
             CS.SetInt("FFTPass", m);
             int ns = 1 << (m - 1);
             CS.SetInt("Ns", ns);
-            ComputeFFT(kernelFFTVertical, ref InputRT);
+            ComputeFFT(kernelFFTVertical, ref InputRT, ref Output);
         }
 
         //进行深度FFT
@@ -125,20 +129,20 @@ public class FFT : MonoBehaviour
             CS.SetInt("FFTPass", m);
             int ns = 1 << (m - 1);
             CS.SetInt("Ns", ns);
-            ComputeFFT(kernelFFTDepth, ref InputRT);
+            ComputeFFT(kernelFFTDepth, ref InputRT, ref Output);
         }
     }
 
-    public void fftshift(ref RenderTexture input)
+    public void fftshift(ref RenderTexture input, ref RenderTexture Output)
     {
         CS.SetInt("N", input.width);
-        ComputeFFT(kernelFFTShift, ref input);
+        ComputeFFT(kernelFFTShift, ref input, ref Output);
     }
 
 
 
-    
-    public void ifft(ref RenderTexture InputRT)
+    [System.Obsolete("This implemenation is possibly buggy")]
+    public void ifft_c(ref RenderTexture InputRT)
     {
         //进行横向FFT
         for (int m = 1; m <= FFTPow; m++)
@@ -192,8 +196,9 @@ public class FFT : MonoBehaviour
         }
     }
 
-    
+
     //TODO: 理论上这应该和 iff() 一样, but 有 bug,,不知道为啥, 留待修复orz 并且 benchmark
+    [System.Obsolete("This implemenation is possibly buggy")]
     public void ifft_a(ref RenderTexture InputRT)
     {
         CS.SetInt("N", InputRT.width);
@@ -269,11 +274,11 @@ public class FFT : MonoBehaviour
     }
 
 
-    public void ifft_c(ref RenderTexture InputRT)
+    public void ifft(ref RenderTexture InputRT, ref RenderTexture Output)
     {
         CS.SetInt("N", InputRT.width);
 
-        ComputeFFT(kernelIFFTConj, ref InputRT);
+        ComputeFFT(kernelIFFTConj, ref InputRT, ref Output);
         //进行横向FFT
         for (int m = 1; m <= FFTPow; m++)
         {
@@ -283,11 +288,11 @@ public class FFT : MonoBehaviour
             //最后一次进行特殊处理
             if (m != FFTPow)
             {
-                ComputeFFT(kernelFFTHorizontal, ref InputRT);
+                ComputeFFT(kernelFFTHorizontal, ref InputRT, ref Output);
             }
             else
             {
-                ComputeFFT(kernelFFTHorizontal_IEND, ref InputRT);
+                ComputeFFT(kernelFFTHorizontal_IEND, ref InputRT, ref Output);
             }
         }
 
@@ -300,11 +305,11 @@ public class FFT : MonoBehaviour
             //最后一次进行特殊处理
             if (m != FFTPow)
             {
-                ComputeFFT(kernelFFTVertical, ref InputRT);
+                ComputeFFT(kernelFFTVertical, ref InputRT, ref Output);
             }
             else
             {
-                ComputeFFT(kernelFFTVertical_IEND, ref InputRT);
+                ComputeFFT(kernelFFTVertical_IEND, ref InputRT, ref Output);
             }
         }
 
@@ -317,11 +322,11 @@ public class FFT : MonoBehaviour
             //最后一次进行特殊处理
             if (m != FFTPow)
             {
-                ComputeFFT(kernelFFTDepth, ref InputRT);
+                ComputeFFT(kernelFFTDepth, ref InputRT, ref Output);
             }
             else
             {
-                ComputeFFT(kernelFFTDepth_IEND, ref InputRT);
+                ComputeFFT(kernelFFTDepth_IEND, ref InputRT, ref Output);
             }
         }
     }
@@ -352,6 +357,8 @@ public class FFT : MonoBehaviour
         kernelFFTShift = CS.FindKernel("FFTShift");
 
         kernelBlitSliceOf3DTexture = CS.FindKernel("BlitSliceOf3DTexture");
+        kernelBlitSliceOf3DTextureFloat4 = CS.FindKernel("BlitSliceOf3DTextureFloat4");
+        kernelBlitSliceOf3DTextureFloat1 = CS.FindKernel("BlitSliceOf3DTextureFloat1");
         kernelIFFTConj = CS.FindKernel("IFFTConj");
     }
 
@@ -364,6 +371,17 @@ public class FFT : MonoBehaviour
         RenderTexture rt = input;
         input = OutputRT;
         OutputRT = rt;
+    }
+
+    void ComputeFFT(int kernel, ref RenderTexture input, ref RenderTexture output)
+    {
+        CS.SetTexture(kernel, "InputRT", input);
+        CS.SetTexture(kernel, "OutputRT", output);
+
+        CS.Dispatch(kernel, N / 8, N / 8, N / 8);
+        RenderTexture rt = input;
+        input = output;
+        output = rt;
     }
 
     Color[] ReadRenderTexture(RenderTexture t, TextureFormat fmt)
@@ -386,16 +404,34 @@ public class FFT : MonoBehaviour
         var res = new float[t.width * t.width * t.volumeDepth*4];
 
         RenderTexture tmp = CreateRenderTexture(t.width, RenderTextureFormat.ARGBFloat);
-        CS.SetTexture(kernelBlitSliceOf3DTexture, "InputRT", t);
-        CS.SetTexture(kernelBlitSliceOf3DTexture, "Sliced", tmp);
+        int kernel;
+        string input;
+        switch(t.format)
+        {
+            case RenderTextureFormat.ARGBFloat:
+                kernel = kernelBlitSliceOf3DTextureFloat4;
+                input = "InputFloat4RT";
+                break;
+            case RenderTextureFormat.RFloat:
+                kernel = kernelBlitSliceOf3DTextureFloat1;
+                input = "InputFloat1RT";
+                break;
+            default:
+                kernel = kernelBlitSliceOf3DTexture;
+                input = "InputRT";
+                break;
+        }
+
+        CS.SetTexture(kernel, input, t);
+        CS.SetTexture(kernel, "Sliced", tmp);
+        
         for(int i = 0; i < t.volumeDepth; ++i)
         {
             CS.SetInt("layer", i);
-            CS.Dispatch(kernelBlitSliceOf3DTexture, t.width / 8, t.width / 8, 1);
+            CS.Dispatch(kernel, t.width / 8, t.width / 8, 1);
             var k = ReadRenderTextureRaw(tmp, TextureFormat.RGBAFloat);
             // k.CopyTo(res, t.width * t.width * i);
             System.Array.Copy(k, 0, res, t.width * t.width * i * 4, k.Length);
-            
         }
         return res;
     }
@@ -425,13 +461,122 @@ public class FFT : MonoBehaviour
         public float[,,] imag;
     };
 
+    struct Float4Array
+    {
+        public float[,,] f1;
+        public float[,,] f2;
+        public float[,,] f3;
+        public float[,,] f4;
+    }
+
+    [System.Serializable]
+    struct Float4Type
+    {
+        public Float4Array data;
+    }
+
     [System.Serializable]
     struct InputType
     {
         public ComplexArray data;
     }
 
-    void ExportArray(RenderTexture t, TextureFormat fmt, int channels, string filename)
+    public void ExportComplex3D(RenderTexture t, string filename)
+    {
+        ExportArray(t, TextureFormat.RGBAFloat, 2, filename);
+    }
+
+    public void ExportFloat4_3D(RenderTexture t, string filename)
+    {
+        var width = t.width;
+        var heigh = t.height;
+        var depth = t.volumeDepth;
+
+        var colors = ReadRenderTextureRaw(t, TextureFormat.RGBAFloat);
+        var index = 0;
+
+        Float4Type ipt;
+        ipt.data.f1 = new float[depth, heigh, width];
+        ipt.data.f2 = new float[depth, heigh, width];
+        ipt.data.f3 = new float[depth, heigh, width];
+        ipt.data.f4 = new float[depth, heigh, width];
+
+        //for(var ch = 0;  ch < channels; ++ch)
+        //{
+        // System.Func<Color, float> get_con = (Color color) => color.a;
+        // if (ch == 0) get_con = (Color color) => color.r;
+        // if (ch == 1) get_con = (Color color) => color.g;
+        // if (ch == 2) get_con = (Color color) => color.b;
+
+        for (int k = 0; k < depth; ++k)
+        {
+            for (int i = 0; i < heigh; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+
+                    ipt.data.f1[k, i, j] = colors[index];
+                    ++index;
+                    ipt.data.f2[k, i, j] = colors[index];
+                    ++index;
+                    ipt.data.f3[k, i, j] = colors[index];
+                    ++index;
+                    ipt.data.f4[k, i, j] = colors[index];
+                    ++index;
+
+                }
+
+            }
+        }
+
+        var chh = Newtonsoft.Json.JsonConvert.SerializeObject(ipt);
+        File.WriteAllText(filename, chh);
+    }
+
+    public void ExportFloat1_3D(RenderTexture t, string filename)
+    {
+        var width = t.width;
+        var heigh = t.height;
+        var depth = t.volumeDepth;
+
+        var colors = ReadRenderTextureRaw(t, TextureFormat.RGBAFloat);
+        var index = 0;
+
+        Float4Type ipt;
+        ipt.data.f1 = new float[depth, heigh, width];
+        ipt.data.f2 = new float[1, 1, 1];
+        ipt.data.f3 = new float[1, 1, 1];
+        ipt.data.f4 = new float[1, 1, 1];
+
+        //for(var ch = 0;  ch < channels; ++ch)
+        //{
+        // System.Func<Color, float> get_con = (Color color) => color.a;
+        // if (ch == 0) get_con = (Color color) => color.r;
+        // if (ch == 1) get_con = (Color color) => color.g;
+        // if (ch == 2) get_con = (Color color) => color.b;
+
+        for (int k = 0; k < depth; ++k)
+        {
+            for (int i = 0; i < heigh; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+
+                    ipt.data.f1[k, i, j] = colors[index];
+                    ++index;
+                    ++index;
+                    ++index;
+                    ++index;
+                }
+
+            }
+        }
+
+        var chh = Newtonsoft.Json.JsonConvert.SerializeObject(ipt);
+        File.WriteAllText(filename, chh);
+    }
+
+    public void ExportArray(RenderTexture t, TextureFormat fmt, int channels, string filename)
     {
         var width = t.width;
         var heigh = t.height;
@@ -538,7 +683,7 @@ public class FFT : MonoBehaviour
 
     public RenderTexture LoadJson3D(string filename, out Texture3D text)
     {
-        var txt = File.ReadAllText("test/input.json");
+        var txt = File.ReadAllText(filename);
         var input = Newtonsoft.Json.JsonConvert.DeserializeObject<InputType>(txt);
         var depth = input.data.real.GetLength(0);
         var height = input.data.real.GetLength(1);
@@ -621,23 +766,34 @@ public class FFT : MonoBehaviour
         ExportArray(textureIn, TextureFormat.RGBAFloat, 2, "test/last_input.json");
         
 
-        fft(ref textureIn);
+        fft(ref textureIn, ref textureOut);
+        
         //Blit2DToDebug(textureIn, DebugOutput);
         ExportArray(textureIn, TextureFormat.RGBAFloat, 2, "test/result.json");
 
-
+        
         Blit3D(texture, textureIn);
         //Graphics.Blit(texture, textureIn);
-        ifft_c(ref textureIn);
+        ifft(ref textureIn, ref textureOut);
+        
         //Blit2DToDebug(textureIn, DebugOutput);
         ExportArray(textureIn, TextureFormat.RGBAFloat, 2, "test/result_ifft.json");
 
-        fftshift(ref textureIn);
+        fftshift(ref textureIn, ref textureOut);
         ExportArray(textureIn, TextureFormat.RGBAFloat, 2, "test/result_ifft_shift.json");
 
-        // Debug.Log(input);
-
         textureIn.Release();
+        textureIn = LoadJson3D("test/input0.json", out texture);
+        ExportComplex3D(textureIn, "test/input0_a.json");
+
+        fft(ref textureIn, ref textureOut);
+        ExportComplex3D(textureIn, "test/result0.json");
+        // Debug.Log(input);
+        textureIn.Release();
+        textureIn = LoadJson3D("test/isf.div.json", out texture);
+        fft(ref textureIn, ref textureOut);
+        ExportComplex3D(textureIn, "test/result.fftdiv.json");
+
         textureOut.Release();
         OutputRT = null;
     }
--- Assets/Scripts/ISF.cs
+++ Assets/Scripts/ISF.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Diagnostics;
+
 public class ISF : MonoBehaviour
 {
     public FFT fft;
@@ -26,7 +28,7 @@ public class ISF : MonoBehaviour
 
     Vector3 size = new Vector3(2, 2, 2);
     float hbar = 0.1f;
-    
+    float estimate_dt = 1f / 30f;
 
     
 
@@ -40,6 +42,8 @@ public class ISF : MonoBehaviour
 
         InitComputeShader();
         InitISF();
+
+        MyRunTest();
     }
 
     void DispatchISFCS(int kernel)
@@ -66,7 +70,7 @@ public class ISF : MonoBehaviour
         SchroedingerMul = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.RGFloat);
         PossionMul = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.RFloat);
         Velocity = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.ARGBFloat);
-        Divergence = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.RFloat);
+        Divergence = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.RGFloat);
         TempRT = FFT.CreateRenderTexture3D(N, N, N, RenderTextureFormat.RGFloat);
 
         fft.OutputRT = TempRT;
@@ -75,26 +79,29 @@ public class ISF : MonoBehaviour
         ISFCS.SetVector("size", size);
         ISFCS.SetInts("res", res);
         ISFCS.SetFloat("hbar", hbar);
+        ISFCS.SetFloat("dt", estimate_dt);
 
         ISFCS.SetTexture(kernelInitBuffer, "SchroedingerMul", SchroedingerMul);
         ISFCS.SetTexture(kernelInitBuffer, "PossionMul", PossionMul);
         DispatchISFCS(kernelInitBuffer);
 
-        fft.fftshift(ref SchroedingerMul);
+        fft.fftshift(ref SchroedingerMul, ref TempRT);
     }
 
     void ShroedingerIntegration(ref RenderTexture psi1, ref RenderTexture psi2)
     {
-        fft.fft(ref psi1);
-        fft.fft(ref psi2);
-
+        fft.fft(ref psi1, ref TempRT);
+        fft.fft(ref psi2, ref TempRT);
+        
         ISFCS.SetTexture(kernelShoedinger, "Psi1", psi1);
         ISFCS.SetTexture(kernelShoedinger, "Psi2", psi2);
         ISFCS.SetTexture(kernelShoedinger, "SchroedingerMul", SchroedingerMul);
         DispatchISFCS(kernelShoedinger);
 
-        fft.ifft(ref psi1);
-        fft.ifft(ref psi2);
+        
+
+        fft.ifft(ref psi1, ref TempRT);
+        fft.ifft(ref psi2, ref TempRT);
     }
 
     void Normalize(ref RenderTexture psi1, ref RenderTexture psi2)
@@ -106,28 +113,73 @@ public class ISF : MonoBehaviour
 
     void PressureProject(ref RenderTexture psi1, ref RenderTexture psi2)
     {
+        ISFCS.SetFloat("hbar", 1);
         // 首先计算 Oneform
         ISFCS.SetTexture(kernelVelocityOneForm, "Psi1", psi1);
         ISFCS.SetTexture(kernelVelocityOneForm, "Psi2", psi2);
         ISFCS.SetTexture(kernelVelocityOneForm, "Velocity", Velocity);
         DispatchISFCS(kernelVelocityOneForm);
 
+        ISFCS.SetFloat("hbar", hbar);
+        // fft.ExportFloat4_3D(Velocity, "test/isf.velo.json");
+
         // 计算散度
         ISFCS.SetTexture(kernelComputeDivergence, "Velocity", Velocity);
         ISFCS.SetTexture(kernelComputeDivergence, "Divergence", Divergence);
         DispatchISFCS(kernelComputeDivergence);
 
+        //fft.ExportComplex3D(Divergence, "test/isf.div.json");
+
         // 求解 Possion 方程
-        fft.fft(ref Divergence);
+        fft.fft(ref Divergence, ref TempRT);
+        // Divergence 比较大, FFT 之后会放大 Divergence, 
+        // float & double 会产生超过 0.02 的精度误差
+
         ISFCS.SetTexture(kernelPossionSpectral, "PossionMul", PossionMul);
         ISFCS.SetTexture(kernelPossionSpectral, "Divergence", Divergence);
         DispatchISFCS(kernelPossionSpectral);
-        fft.ifft(ref Divergence);
+        
+        fft.ifft(ref Divergence, ref TempRT);
+        //fft.ExportComplex3D(Divergence, "test/isf.pos.json");
+
+        //fft.ExportFloat1_3D(PossionMul, "test/isf.fac.json");
 
         ISFCS.SetTexture(kernelGaugeTransform, "Psi1", psi1);
         ISFCS.SetTexture(kernelGaugeTransform, "Psi2", psi2);
         ISFCS.SetTexture(kernelGaugeTransform, "Divergence", Divergence);
         DispatchISFCS(kernelGaugeTransform);
+    }
+
+    void MyRunTest()
+    {
+        Texture3D tex_psi1;
+        Texture3D tex_psi2;
+
+        var psi1 = fft.LoadJson3D("test/psi1.json", out tex_psi1);
+        var psi2 = fft.LoadJson3D("test/psi2.json", out tex_psi2);
+
+        fft.ExportComplex3D(psi1, "test/sch.ps1.fft.json");
+
+        ShroedingerIntegration(ref psi1, ref psi2);
+        
+
+        fft.ExportArray(SchroedingerMul, TextureFormat.RGBAFloat, 2, "test/isf.sch.mul.json");
+
+        fft.ExportArray(psi1, TextureFormat.RGBAFloat, 2, "test/isf.sch.ps1.json");
+        fft.ExportArray(psi2, TextureFormat.RGBAFloat, 2, "test/isf.sch.ps2.json");
+
+        Normalize(ref psi1, ref psi2);
+
+        
+        fft.ExportArray(psi1, TextureFormat.RGBAFloat, 2, "test/isf.nor.ps1.json");
+        fft.ExportArray(psi2, TextureFormat.RGBAFloat, 2, "test/isf.nor.ps2.json");
+
+        PressureProject(ref psi1, ref psi2);
+
+        fft.ExportArray(psi1, TextureFormat.RGBAFloat, 2, "test/isf.pre.ps1.json");
+        fft.ExportArray(psi2, TextureFormat.RGBAFloat, 2, "test/isf.pre.ps2.json");
+
+        
     }
 
     // Update is called once per frame
